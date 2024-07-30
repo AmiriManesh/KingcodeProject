a184c3e1b84310a7c8ae41d0ce9dca039d6bee0c
Hamid AmiriManesh <devamirimanesh@gmail.com>
2024-07-30 11:06:10 +0330

    Add(WASD Movement Code)

--- Assets/Scripts/Player/PlayerInputSystem/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem/PlayerInputSystem.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody playerRigidBody;
+    private PlayerInputAction playerInputAction;
+    private float speed = 10f;
+    
+    private void Awake()
     {
-        
+        GetComps();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerInputAction.Player.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = playerInputAction.Player.Movement.ReadValue<Vector2>();
+        playerRigidBody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
+    }
+
+    private void GetComps()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        playerInputAction = new PlayerInputAction();
     }
 }
